902abebc8dfbb19249ad99d5ff7b636304d6eb3c
tingm1020 <58199984+tingm1020@users.noreply.github.com>
2021-08-06 05:05:40 +0800

    0806_js upload
    0806_js upload

---
+++ js/common.js
@@ -0,0 +1,139 @@
+
+var mySwiper1 = new Swiper('.swiper-container1', {
+        initialSlide:0,
+        observer:true,
+        observeParents:true,
+        effect: 'slide',
+        slidesPerView: 4.5,
+        spaceBetween:40,
+        centeredSlides: true,
+        loop: true,
+        initialSlide: 0,
+        freeMode: true,
+        navigation: {
+            nextEl: '.swiper-button-next1',
+            prevEl: '.swiper-button-prev1',
+        },
+        pagination: {
+            el: '.swiper-pagination1',
+        },
+        on: {
+            resize: function () {
+                    setTimeout(() => {
+                    mySwiper1.update()
+                    }, 500) 
+                    }
+            },
+        breakpoints:{
+            1281:{
+                slidesPerView: 3.5,
+                spaceBetween:20,
+            },
+            735:{
+                slidesPerView: 2.5,
+                spaceBetween:20,
+            },
+            414:{
+                slidesPerView: 1,
+                spaceBetween:0,
+            }
+
+        },
+        speed: 600,
+        preloadImages: true,
+        
+}); 
+var mySwiper2 = new Swiper('.swiper-container2', {
+    initialSlide:0,
+    observer:true,
+    observeParents:true,
+    effect: 'slide',
+    slidesPerView: 1,
+    spaceBetween:20,
+    centeredSlides: true,
+    // loop: true,
+    initialSlide: 0,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+    },
+    pagination: {
+        el: '.swiper-pagination2',
+    },
+    
+    speed: 400,
+    preloadImages: true,
+
+}); 
+
+
+$(document).ready(function(){
+    function addRemoveClass() {
+        var delay = 2250;
+        setTimeout(function() {
+            $('.img_animate .img').removeClass('on');
+            addRemoveClass();
+        }, delay);
+    };
+    function imgAnimate(){
+        var $imgAnimation = $('.img_animate .img'), counter = 0;
+        setTimeout(function(){
+            setInterval(function(){
+                if ($imgAnimation) clearInterval($imgAnimation);
+                $imgAnimation.eq(counter++ % $imgAnimation.length).addClass('on');
+            },250);
+            addRemoveClass();
+            
+        });
+    }
+    imgAnimate();
+    var popOn = $('.header,.main,.popup,.contBox');
+    $('.xBtn').click(function(){
+        if($(popOn).hasClass('on')){
+            $(popOn).removeClass('on');
+            $('html,body').animate({
+                scrollTop: $('.page5').offset().top + 120
+            }, 500);
+        } else{
+            $(popOn).addClass('on');
+        }
+    });
+    $('.clickPop').click(function(){
+        var index = $(this).index();
+        mySwiper2.slideTo(index);
+        $(popOn).addClass('on');
+        
+    });
+    
+    var $loading = $('.loading')
+    window.onload = function() {
+        $loading.removeClass('on');
+    }
+    window.onresize = function(){
+        $loading.removeClass('on');
+        document.location.reload(true);
+    }
+    $(window).scroll(function () {
+        var page5 = $('.page4').height() * .5;
+        if (getScrollTop() > page5) {
+            $('.page5 .img_block').addClass('animated');
+        } else {
+            $('.page5 .img_block').removeClass('animated');
+        }
+    });
+    function getScrollTop() {
+        var bodyTop = 0;
+        if (typeof window.pageY0ffset != "undefined") {
+            bodyTop = window.pageY0ffset;
+        } else if (typeof document.compatMode != "undefined" &&
+            document.compatMode != "BackCompat") {
+            bodyTop = document.documentElement.scrollTop;
+        } else if (typeof document.body != "undefined") {
+            bodyTop = document.body.scrollTop;
+        }
+        return bodyTop;
+    }
+    $(window).resize(function () { });
+
+});  
